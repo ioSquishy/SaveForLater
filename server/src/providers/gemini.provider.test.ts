8f6901f69cc2b34b85af4a4e4622abef68c71cd3
ioSquishy <111015917+ioSquishy@users.noreply.github.com>
2026-03-21 16:09:05 -0700

    added gemini provider tests with vitest

---
+++ server/src/providers/gemini.provider.test.ts
@@ -0,0 +1,52 @@
+import * as fs from "node:fs";
+import { expect, test } from 'vitest'
+import { getTrackFromBase64 } from "./gemini.provider";
+import Mime from "../types/mime";
+
+async function testLocalImage(pathToImg: string, mimeType: Mime) {
+  const base64ImageFile = fs.readFileSync(pathToImg, {
+    encoding: "base64",
+  });
+  
+  try {
+    return await getTrackFromBase64(base64ImageFile, mimeType);
+  } catch (e) {
+    throw e;
+  }
+}
+
+test('multiple artists', async () => {
+  let result = await testLocalImage("test/track_images/artists.jpg", Mime.JPEG);
+  expect(result).toMatchObject({
+    songTitle: 'More Than Ever',
+    songArtists: [ 'Andrea Chahayed', 'Jackson Rau' ]
+  });
+  expect(result?.certainty).toBeGreaterThan(0.5);
+});
+
+test('full screenshot', async () => {
+  let result = await testLocalImage("test/track_images/full.PNG", Mime.PNG);
+  expect(result).toMatchObject({
+    songTitle: 'ONE LAST TIME',
+    songArtists: [ 'COOING' ]
+  });
+  expect(result?.certainty).toBeGreaterThan(0.5);
+});
+
+test('foreign language', async () => {
+  let result = await testLocalImage("test/track_images/lang.jpg", Mime.JPEG);
+  expect(result).toMatchObject({
+    songTitle: '東京劇場',
+    songArtists: [ 'Ettone' ]
+  });
+  expect(result?.certainty).toBeGreaterThan(0.5);
+});
+
+test('simple', async () => {
+  let result = await testLocalImage("test/track_images/simple.jpg", Mime.JPEG);
+  expect(result).toMatchObject({
+    songTitle: 'kiss me blue',
+    songArtists: [ 'pami' ]
+  });
+  expect(result?.certainty).toBeGreaterThan(0.5);
+});
